62319c5e2d0e1336d9d6414e48969a289df4d078
Eric Yulianto <eric.yulianto.ang@gmail.com>
2014-05-06 14:59:16 +0800

    Login view done

--- accounts/tests/test_views.py
+++ accounts/tests/test_views.py
@@ -1,5 +1,7 @@
+from django.contrib.auth import get_user_model, SESSION_KEY
 from django.test import TestCase
 from unittest.mock import patch
+User = get_user_model()
 
 
 class LoginViewTest(TestCase):
@@ -12,3 +14,34 @@ class LoginViewTest(TestCase):
         mock_authenticate.return_value = None
         self.client.post('/accounts/login', {'assertion': 'assert this'})
         mock_authenticate.assert_called_once_with(assertion='assert this')
+
+    @patch('accounts.views.authenticate')
+    def test_return_OK_when_user_found(
+            self,
+            mock_authenticate
+    ):
+        user = User.objects.create(email='a@b.com')
+        user.backend = ''
+        mock_authenticate.return_value = user
+        response = self.client.post('/accounts/login', {'assertion': 'a'})
+        self.assertEqual(response.content.decode(), 'OK')
+
+    @patch('accounts.views.authenticate')
+    def test_gets_logged_in_session_if_authenticate_returns_a_user(
+            self,
+            mock_authenticate
+    ):
+        user = User.objects.create(email='a@b.com')
+        user.backend = ''
+        mock_authenticate.return_value = user
+        self.client.post('/accounts/login', {'assertion': 'a'})
+        self.assertEqual(self.client.session[SESSION_KEY], user.pk)
+    
+    @patch('accounts.views.authenticate')
+    def test_does_not_get_logged_in_if_authenticate_returns_None(
+            self,
+            mock_authenticate
+    ):
+        mock_authenticate.return_value = None
+        self.client.post('/accounts/login', {'assertion': 'a'})
+        self.assertNotIn(SESSION_KEY, self.client.session)
