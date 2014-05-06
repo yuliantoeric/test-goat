446ce280641cb6bf4ea82e243d0f531351a473d5
Eric Yulianto <eric.yulianto.ang@gmail.com>
2014-05-06 14:44:16 +0800

    First python mock

---
+++ accounts/views.py
@@ -0,0 +1,6 @@
+from django.contrib.auth import authenticate
+from django.http import HttpResponse
+
+def persona_login(request):
+    authenticate(assertion=request.POST['assertion'])
+    return HttpResponse()
--- superlists/settings.py
+++ superlists/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'lists',
+    'accounts',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -85,3 +86,20 @@ STATIC_ROOT = os.path.join(BASE_DIR, '../static')
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'superlists', 'static'),
 )
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+        },
+    },
+    'root': {'level': 'INFO'},
+}
