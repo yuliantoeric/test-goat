f6df1ce4ba44f75187307173fb90a9822689fe85
Eric Yulianto <eric.yulianto.ang@gmail.com>
2014-05-06 19:11:06 +0800

    slightly more isolated tests at view layer

--- lists/views.py
+++ lists/views.py
@@ -3,6 +3,8 @@ from django.core.exceptions import ValidationError
 from lists.models import Item, List
 from lists.forms import ExistingListItemForm, ItemForm
 
+from django.contrib.auth import get_user_model
+User = get_user_model()
 
 def home_page(request):
     return render(request, 'home.html', {'form': ItemForm()})
@@ -22,7 +24,9 @@ def view_list(request, list_id):
 def new_list(request):
     form = ItemForm(data=request.POST)
     if form.is_valid():
-        list_ = List.objects.create()
+        list_ = List()
+        list_.owner = request.user
+        list_.save()
         form.save(for_list=list_)
         return redirect(list_)
     else:
@@ -30,4 +34,5 @@ def new_list(request):
 
 
 def my_lists(request, email):
-    return render(request, 'my_lists.html')
+    owner = User.objects.get(email=email)
+    return render(request, 'my_lists.html', {'owner': owner})
